7421faeba9fca59c20004d88ff483f58e386e2a4
ShrimantMali <114905933+ShrimantMali@users.noreply.github.com>
2022-10-16 14:07:17 +0530

    Add files via upload

---
+++ LoginDAO.java
@@ -0,0 +1,32 @@
+package com.javabykiran.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.javabykiran.entity.Users;
+
+@Repository
+public class LoginDAO {
+
+	@Autowired
+	SessionFactory factory;
+	Session session;
+	
+	
+	public String getPassword(String username) {
+		
+		session=factory.openSession();
+		System.out.println("session created..");
+		
+		System.out.println(username);
+		Users user=session.get(Users.class,username);  
+		if(user==null)
+			return null;
+		else
+			
+		return user.getPassword();
+	}
+
+}
